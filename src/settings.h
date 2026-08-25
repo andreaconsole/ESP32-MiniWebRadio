6aa64e165eb20001ebdef15b0ee80c0a5d6db537
andreaconsole <andreaconsole@gmail.com>
2026-08-25 13:57:54 +0200

    Set Bluetooth emitter pins to -1 for unavailability
    Updated Bluetooth emitter pin definitions to -1 indicating no availability.

--- src/settings.h
+++ src/settings.h
@@ -146,14 +146,14 @@
         #define I2S_BCLK           3
         #define I2S_LRC            1
         #define I2S_MCLK           0
-        #define BT_EMITTER_RX      45 // TX pin - KCX Bluetooth Transmitter    (-1 if not available)
-        #define BT_EMITTER_TX      38 // RX pin - KCX Bluetooth Transmitter    (-1 if not available)
-        #define BT_EMITTER_MODE    20 // high transmit - low receive           (-1 if not available)
+        #define BT_EMITTER_RX      -1 // TX pin - KCX Bluetooth Transmitter    (-1 if not available) -AC- no serial comm available
+        #define BT_EMITTER_TX      -1 // RX pin - KCX Bluetooth Transmitter    (-1 if not available)
+        #define BT_EMITTER_MODE    -1 // high transmit - low receive           (-1 if not available)
         #define BT_EMITTER_CONNECT 48 // high impulse -> awake after POWER_OFF (-1 if not available)
         #define I2C_SDA            41 // I2C, dala line for capacitive touchpadand and light sensor (-1 if not available)
         #define I2C_SCL            42 // I2C, clock line for capacitive touchpadand and light sensor (-1 if not available)
         #define AMP_ENABLED        -1 // onboard amplifier (-1 if not available)
-        #define AUDIO_SWITCH        46 // -AC- switch between radio and BT receiver (-1 if not available) 
+        #define AUDIO_SWITCH       46 // -AC- switch between radio and BT receiver (-1 if not available) 
     #endif                            // CONFIG_IDF_TARGET_ESP32S3
 
     #if CONFIG_IDF_TARGET_ESP32P4
