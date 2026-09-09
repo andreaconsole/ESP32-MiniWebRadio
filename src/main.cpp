0337e246a5e5434487c395372241d7352c4d7c9b
andreaconsole <andreaconsole@gmail.com>
2026-09-09 14:38:03 +0200

    Update version information in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
     MiniWebRadio -- Webradio receiver for ESP32-S3
 
     first release on 03/2017                                                                                                      */char Version[] ="\
-    Version 4.2.0t2 - Aug 26, 2026                                                                                                               ";
+    V 1.0.0 - 09 Sept 2026 (Forked from 4.2.0t2)                                                                                                     ";
 
 /*  display (320x240px) with controller ILI9341 or
     display (480x320px) with controller ILI9486, ILI9488 or ST7796 (SPI) or
